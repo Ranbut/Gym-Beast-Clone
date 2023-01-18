c1e856ee152958ff332007e59357db5cf739f616
Ranbut <Ranbut_Beater@protonmail.com>
2023-01-18 19:28:40 -0300

    Added 3D movement and animation to player

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private CharacterController controller;
+    private Animator animator;
+
+    public Transform camera;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+
+        animator.SetFloat("Speed", direction.magnitude);
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 movDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(movDir * speed * Time.deltaTime);
+        }
     }
 }
